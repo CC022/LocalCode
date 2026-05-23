54f88411f4eb7099fe011254a59959ff77731902
CC022 <w86f9x@gmail.com>
2026-05-22 21:18:21 -0700

    Initial Commit

---
+++ localCode/localCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  localCodeApp.swift
+//  localCode
+//
+//  Created by zzc on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct localCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
